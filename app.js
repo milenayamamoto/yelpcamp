8d3c7c015c34c5774ca9c9e79c5e4ffd38f66d66
root <milenayamamoto@gmail.com>
2019-10-04 04:16:35 +0000

    update app

--- app.js
+++ app.js
@@ -18,8 +18,15 @@ var commentRoutes 		= require("./routes/comments"),
 	campgroundRoutes 	= require("./routes/campgrounds"),
 	indexRoutes 		= require("./routes/index")
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v14", { useNewUrlParser: true });
-mongoose.set('useCreateIndex', true);
+mongoose.connect(process.env.DATABASEURL, { 
+	useNewUrlParser: true,
+	useCreateIndex: true
+}).then(() => {
+	console.log("Connected to DB!");
+}).catch(err => {
+	console.log("ERROR: ", err.message);
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -30,7 +37,7 @@ app.locals.moment = require('moment');
 
 //PASSPORT CONFIGURATION
 app.use(require("express-session")({
-	secret: "Once again Rusty wins cutest dog!",
+	secret: "This is an express session!",
 	resave: false,
 	saveUninitialized: false
 }));
